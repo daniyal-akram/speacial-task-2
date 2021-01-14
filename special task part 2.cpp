ca1a523147dede5fc0ab80b072ebe06cb5a07db2
Daniyal_Akram_07 <77106224+daniyal-akram@users.noreply.github.com>
2021-01-14 16:44:16 +0500

    Add files via upload

---
+++ special task part 2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int length,breadth;
+ cout<<"Entre the Length :";
+ cin>>length;
+ cout<<"Entre the Breadth:";
+ cin>>breadth;
+ cout<<"Area of reactangle="<<length*breadth<<endl;
+ } 
